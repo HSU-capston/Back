c8830a7ee6334593dcfa8458c4a7ee2db9bf59d2
Santoragi32 <woojj1254577@naver.com>
2025-03-12 14:47:11 +0900

    [#7]Refactor: 로컬 경로 변수 변경

--- src/main/java/capstone/SportyUp/SportyUp_Server/config/WebConfig.java
+++ src/main/java/capstone/SportyUp/SportyUp_Server/config/WebConfig.java
@@ -1,15 +1,20 @@
 package capstone.SportyUp.SportyUp_Server.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${local-path.result}")
+    private String resultPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/processed-files/**")
-                .addResourceLocations("file:C:/Users/EliteBook/Documents/GitHub/Back/src/main/resources/cam_after_flask/");
+                .addResourceLocations("file:"+resultPath);
 //        registry.addResourceHandler("/Test/**") 바탕화면에서 테스트
 //                .addResourceLocations("file:C:/Users/EliteBook/Desktop/");
     }
