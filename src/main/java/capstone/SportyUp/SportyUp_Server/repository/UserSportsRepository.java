1f95170e34fb82eb72d285362e316acec762f7f1
Santoragi32 <woojj1254577@naver.com>
2025-04-16 14:31:20 +0900

    [#66]Bug: UserSportsRepository에서 하나의 값만 받아오도록 수정

--- src/main/java/capstone/SportyUp/SportyUp_Server/repository/UserSportsRepository.java
+++ src/main/java/capstone/SportyUp/SportyUp_Server/repository/UserSportsRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserSportsRepository extends JpaRepository<UserSports, Long> {
-    Optional<UserSports> findByUser(User user);
+    Optional<UserSports> findTop1ByUser(User user);
 }
