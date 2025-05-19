51c8ee08248da8c7a23b8382b86107d864855079
Santoragi32 <woojj1254577@naver.com>
2025-05-19 14:30:27 +0900

    [#78]Feat: game responseDTO 수정

--- src/main/java/capstone/SportyUp/SportyUp_Server/web/DTO/GameDTO/GameResponseDTO.java
+++ src/main/java/capstone/SportyUp/SportyUp_Server/web/DTO/GameDTO/GameResponseDTO.java
@@ -44,7 +44,7 @@ public class GameResponseDTO {
     @AllArgsConstructor
     public static class GameDetailDTO{
         Long id;
-        LocalDateTime playDate;
+//        LocalDateTime playDate;
         String summary; //게임 요약
         Integer score;
         String highlightUrl;    //하이라이트 url
